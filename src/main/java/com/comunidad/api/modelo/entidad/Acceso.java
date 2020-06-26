47aff8dd73e0685aa524e028f5fe66ed435452f8
V8Sebastian <bsebastianvg18@gmail.com>
2020-06-25 20:30:26 -0500

    creando estructura basica de clases de las entidades Acceso, AccesoVisitante y Basico

--- src/main/java/com/comunidad/api/modelo/entidad/Acceso.java
+++ src/main/java/com/comunidad/api/modelo/entidad/Acceso.java
@@ -1,7 +1,84 @@
 package com.comunidad.api.modelo.entidad;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "acceso")
 public class Acceso implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@OneToOne
+	@JoinColumn(unique = true)
+	// @JsonIgnoreProperties
+	private Registro registro;
+	@Column(name = "fechareg")
+	private Date fechaReg;
+	private Long estado;
+	private static final long serialVersionUID = 1L;
+
+	public Acceso() {
+		super();
+	}
+
+	public Acceso(Long id, Registro registro, Date fechaReg, Long estado) {
+		super();
+		this.id = id;
+		this.registro = registro;
+		this.fechaReg = fechaReg;
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Acceso [id=" + id + ", registro=" + registro + ", fechaReg=" + fechaReg + ", estado=" + estado + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Registro getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(Registro registro) {
+		this.registro = registro;
+	}
+
+	public Date getFechaReg() {
+		return fechaReg;
+	}
+
+	public void setFechaReg(Date fechaReg) {
+		this.fechaReg = fechaReg;
+	}
+
+	public Long getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Long estado) {
+		this.estado = estado;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
--- src/main/java/com/comunidad/api/modelo/entidad/AccesoVisitante.java
+++ src/main/java/com/comunidad/api/modelo/entidad/AccesoVisitante.java
@@ -1,7 +1,85 @@
 package com.comunidad.api.modelo.entidad;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "accesovisitante")
 public class AccesoVisitante implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date fecha;
+	private Long temperatura;
+	@OneToOne
+	@JoinColumn(unique = true)
+	// @JsonIgnoreProperties
+	private Visitante visitante;
+	private static final long serialVersionUID = 1L;
+
+	public AccesoVisitante() {
+		super();
+	}
+
+	public AccesoVisitante(Long id, Date fecha, Long temperatura, Visitante visitante) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.temperatura = temperatura;
+		this.visitante = visitante;
+	}
+
+	@Override
+	public String toString() {
+		return "AccesoVisitante [id=" + id + ", fecha=" + fecha + ", temperatura=" + temperatura + ", visitante="
+				+ visitante + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Long getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(Long temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public Visitante getVisitante() {
+		return visitante;
+	}
+
+	public void setVisitante(Visitante visitante) {
+		this.visitante = visitante;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
